8a82e583fca1c8a905b3f3c9a8faa6977328ee22
duypham228 <duyphamm228@gmail.com>
2024-12-06 01:08:28 -0600

    nodemon for dev

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Server is ready");
+});
+
 app.listen(5000, () => {
   console.log("Server is running on http://localhost:5000");
 });
