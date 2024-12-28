5d81522b1eb1d54f6b29ac162d8c64f47af75381
duypham228 <duyphamm228@gmail.com>
2024-12-28 00:16:31 -0600

    files for routes and controllers

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export async function signup(req, res) {
+  res.send("Signup route");
+}
+
+export async function signin(req, res) {
+  res.send("Signin route");
+}
+
+export async function signout(req, res) {
+  res.send("Signout route");
+}
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 import express from "express";
 
+import autRoutes from "./routes/auth.route.js";
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Server is ready");
-});
+app.use("/api/v1/auth", autRoutes);
 
 app.listen(5000, () => {
   console.log("Server is running on http://localhost:5000");
