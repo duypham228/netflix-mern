797f02d4be345c23a0879d0136642131aac9e213
duypham228 <duyphamm228@gmail.com>
2024-12-31 17:45:13 -0600

    signup route tested with postman

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,3 +1,5 @@
+import { User } from "../models/user.model.js";
+
 export async function signup(req, res) {
   try {
     const { username, email, password } = req.body;
@@ -9,7 +11,52 @@ export async function signup(req, res) {
     }
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  } catch (error) {}
+
+    if (!emailRegex.test(email)) {
+      return res.status(400).json({ success: false, error: "Invalid email" });
+    }
+
+    if (password.length < 6) {
+      return res.status(400).json({
+        success: false,
+        error: "Password must be at least 6 characters",
+      });
+    }
+
+    const existingUserByEmail = await User.findOne({ email });
+
+    if (existingUserByEmail) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Email already exists" });
+    }
+
+    const existingUserByUsername = await User.findOne({ username });
+
+    if (existingUserByUsername) {
+      return res
+        .status(400)
+        .json({ success: false, error: "Username already exists" });
+    }
+
+    const PROFILE_PICS = ["/avatar1.png", "/avatar2.png", "/avatar3.png"];
+    const image = PROFILE_PICS[Math.floor(Math.random() * PROFILE_PICS.length)];
+
+    const newUser = new User({ username, email, password, image });
+
+    await newUser.save();
+
+    res.status(201).json({
+      success: true,
+      user: {
+        ...newUser._doc,
+        password: "",
+      },
+    });
+  } catch (error) {
+    console.error(`Error in signup controller: ${error.message}`);
+    res.status(500).json({ success: false, error: "Internal Server Error" });
+  }
 }
 
 export async function signin(req, res) {
